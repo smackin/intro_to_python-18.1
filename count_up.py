74132d7beb4b4351984d452464ce1aa7399b8200
Stephanie <smmackin12@gmail.com>
2022-01-10 13:27:16 -0500

    solution added

--- count_up.py
+++ count_up.py
@@ -1,18 +1,7 @@
 def count_up(start, stop):
-    """Print all numbers from start up to and including stop.
 
-    For example:
+    while start <= stop:    #while the start int is less than or equal to the stop int we want to print the start int and then add 1 -  run int through start <= stop - if yes, print start and then add 1,  if no, breaks out.  
+        print(start)
+        start = start + 1 
 
-        count_up(5, 7)
-
-   should print:
-
-        5
-        6
-        7
-    """
-
-    # YOUR CODE HERE
-
-
-count_up(5, 7)        
+count_up(5, 10)        
